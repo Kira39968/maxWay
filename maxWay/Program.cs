5945a10417db2bb1c1933213eead3ac193858b93
Кирилл <kovkiralex@gmail.com>
2016-10-09 15:20:42 +0300

    release

---
+++ maxWay/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxWay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient myWebClient = new WebClient();
+            myWebClient.DownloadFile("https://dl.dropboxusercontent.com/u/28873424/tasks/simple_triangle.txt", "TriangleToParsing.txt");
+            Parser parseTriangle = new Parser("TriangleToParsing.txt");
+            Console.WriteLine("The max way of triangle is " + parseTriangle.MaxWayOfTriangle());
+            Console.ReadKey();
+        }
+    }
+}
